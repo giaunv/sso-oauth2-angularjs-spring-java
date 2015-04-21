ad0fa5e1929b7c3750283f5064bc9c11054f3821
Giau Ngo <giaunv@gmail.com>
2015-04-21 17:22:37 +0700

    Login

--- ui/src/main/java/demo/UiApplication.java
+++ ui/src/main/java/demo/UiApplication.java
@@ -2,9 +2,15 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -12,6 +18,10 @@ import java.util.UUID;
 @SpringBootApplication
 @RestController
 public class UiApplication {
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
 
     @RequestMapping("/resource")
     public Map<String,Object> home() {
@@ -19,6 +29,20 @@ public class UiApplication {
         model.put("id", UUID.randomUUID().toString());
         model.put("content", "Hello World");
         return model;
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+          http
+            .httpBasic()
+          .and()
+            .authorizeRequests()
+              .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+              .anyRequest().authenticated();
+        }
     }
 
     public static void main(String[] args) {
